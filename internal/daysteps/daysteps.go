d823ee99c73af74d9a042d5714052a8be7ed3700
Malykhin Ivan <ivan.malykhin.29@mail.ru>
2026-05-04 01:26:07 +0500

    third commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -41,7 +41,5 @@ func (ds DaySteps) ActionInfo() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf(`Количество шагов: %d.
-						Дистанция составила %.2f км.
-						Вы сожгли %.2f ккал.`, ds.Steps, distance, calories), nil
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", ds.Steps, distance, calories), nil
 }
